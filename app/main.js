098724c9dc96915af5d5de4bff17915306f0bb35
Lakshya Mittal <lakshyamittalaka@gmail.com>
2024-03-11 01:08:40 +0530

    Configure listening port

--- app/main.js
+++ app/main.js
@@ -3,7 +3,14 @@ const MasterServer = require("./MasterServer");
 const HOST = 'localhost';
 const PORT = '6379';
 
-(function init(){
-    let server = new MasterServer(HOST, PORT);
-    server.startServer();
-})()
+(function init(args){
+    if(args.length === 0){
+        let server = new MasterServer(HOST, PORT);
+        server.startServer();
+    } else{
+        let portFlag = args[0];
+        let port = args[1];
+        let server = new MasterServer(HOST, port);
+        server.startServer();
+    }
+})(process.argv.slice(2))
